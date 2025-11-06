c13451aeb47134ea6bb2ddb1b18a0805f011e379
BuildTools <unconfigured@null.spigotmc.org>
2025-11-06 02:25:58 -0800

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/ui/songs/SongsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.musicplayer.ui.songs
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SongsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is songs Fragment"
+    }
+    val text: LiveData<String> = _text
+}
